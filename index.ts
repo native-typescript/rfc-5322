rafal-majewski/template-of-node-js-library

feature!: Kick-off the library

index.ts
@@ -1,0 +1,1 @@
+export type * as templateOfNodeJsLibrary from "./source/index.ts";

native-typescript/rfc-5322

index.ts
@@ -1,0 +1,1 @@
+export type * as rfc5322 from "./source/index.ts";